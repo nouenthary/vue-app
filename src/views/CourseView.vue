9283c4ffdd1ef645078e61be161c062d51fba3c4
Nouen Thary <54056498+nouenthary@users.noreply.github.com>
2024-12-09 16:14:42 +0700

    Update CourseView.vue

--- src/views/CourseView.vue
+++ src/views/CourseView.vue
@@ -1,12 +1,36 @@
 <template>
     <div>
         Course Vue
+
+        <div>
+            <pre>
+                {{data}}
+            </prev>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "CourseView"
+        name: "CourseView",
+        data(){
+            return {
+                data:[],
+            };
+        },
+        methods: {
+            getData(){
+                fetch('https://fakestoreapi.com/products/category/jewelery')
+                .then(res=>res.json())
+                .then(json=>{
+                    this.data = json;
+                    console.log(json);
+                });
+            }
+        },
+        created(){
+            this.getData();
+        }
     }
 </script>
 
